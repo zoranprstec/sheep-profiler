bd9926874c6f985ee8f260801b89cc238eaa9497
Zoran Prstec <zoran.prstec@gmail.com>
2022-06-14 17:19:45 +0200

    navbar CSS

--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,13 +1,28 @@
-import { Link, Outlet } from "react-router-dom";
+import { SyntheticEvent, useEffect } from "react"
+import { Link, Outlet, useLocation } from "react-router-dom"
+import "./Layout.css"
 
 export default function Layout() {
+    const location = useLocation()
+    const navlinks = document.getElementsByClassName("navlink")
+    
+    useEffect(() => {
+        var i = 0
+        for (i = 0; i < navlinks.length; i++) {
+            navlinks[i].className = navlinks[i].className.replace(" activelink", "")
+            if (navlinks[i].id === location.pathname) {
+                navlinks[i].className += " activelink"
+            }
+        }
+    }, [location, navlinks])
+    
     return (
         <div>
-            <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/sheep-manager">Manage Sheep</Link></li>
-          </ul>
+            <div className="navbar">
+                <Link className="navlink" id="/" to="/">Home</Link>
+                <Link className="navlink" id="/about" to="/about">About</Link>
+                <Link className="navlink" id="/sheep-manager" to="/sheep-manager">Manager</Link>
+            </div>
           <Outlet />
         </div>
     )
