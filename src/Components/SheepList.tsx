c12b18733d8dd6243596a5005dbf5e606c4eb488
Zoran Prstec <zoran.prstec@gmail.com>
2022-07-13 12:50:08 +0200

    Fixed the rendering of sheep thumbnail. Still work in progress.

--- src/Components/SheepList.tsx
+++ src/Components/SheepList.tsx
@@ -1,27 +1,99 @@
-import { useDB, useFind } from "react-pouchdb"
+import { useEffect, useState, memo } from "react"
+import { useDB, useFind} from "react-pouchdb"
 import "./SheepList.css"
 
-export default function SheepList() {
+interface sheepTypes {
+    _id: string,
+    name: string,
+    dateOfBirth: string,
+    description: string,
+    picture: string
+}
+
+function SheepList() {
+    const [src, setSrc] = useState([""])
+    const [sheepArray, setSheepArray] = useState([]) 
+    const [sheepNode, setSheepNode] = useState([])
     const db = useDB('sheep_database')
+
+    /**
+     * TODO:
+     * - [X] useFind se izvrši svaki put kad imamo re-render, izvrši samo kad se promijeni "db"
+     * - [X] db.getAttachment izvrti preko "for each" na sheepList i onda rezultat priključi na
+     *      state u kojem se drži sheepList
+     * - [ ] istraži nešto kao db.updated, neka metoda koja se izvršavva kad se db updejta
+     *      možda db slati u ovu komponentu kao prop?
+     */ 
+    
+    // db.getAttachment("Franka", "sheep-pic.jpg")
+    // .then((blob: any) => {
+    //     console.log(blob)
+    //     let url = URL.createObjectURL(blob)
+    //     setSrc(url)
+    // })
+    // .catch((err: any) => {
+    //     console.log(err)
+    // })
 
     const sheepList = useFind(db, {
         selector: {
             name: { $gte: null }      // $gte znači "greater than or equal"; pronalazi sve entryje
         },
-        sort: ["name"]
+        sort: ["name"],
+        attachments: true
     })
-      .map((sheep: any) => (
-        <li key={sheep._id}>
+    
+    if (sheepList.length > 0) {
+        for (const sheep of sheepList) {
+
+            db.getAttachment(sheep._id, "sheeppic.jpg")
+            .then((blob: any) => {
+                var reader = new FileReader();
+                reader.readAsDataURL(blob); 
+                reader.onloadend = () => {
+                    Object.defineProperty(sheep, 'picture', {
+                        value: reader.result as string
+                    })}
+                })
+            .catch((err: any) => {
+                console.log(err)
+            })
+        }
+    }
+
+    useEffect(() => {
+        setSheepArray(sheepList)
+        console.log("sheepArray set")
+    }, [db])
+
+
+    // console.log(sheepArray)
+    
+    // Ovo ne ide u useEffect, jer je ovo "useFind"; hooks ne smijemo stavljati u callback.
+    // Hooks moraju biti Top level.
+    
+    // useEffect(() => {
+    //     setSheepArray(sheepList)
+    // }, [])
+    
+    const sheepListed = sheepArray.map((sheep: sheepTypes) => {
+        return (
+            <li key={sheep._id}>
+            <img width={"100px"} alt="sheepic" src={sheep.picture}></img>
             <p>{sheep.name}</p>
-            <p>{sheep.dateOfBirth}</p>
+            <p>{new Date(sheep.dateOfBirth).toLocaleDateString()}</p>
             <p>{sheep.description}</p>
             <button onClick={() => db.remove(sheep)}>Remove</button>
         </li>
-    ))
+    )})
 
+    console.log("SheepList component rendered")
+    
     return (
         <ul className="sheep">
-            {sheepList}
+            {sheepListed}
         </ul>
     )
 }
+
+export default memo(SheepList)
--- src/Pages/SheepManager.tsx
+++ src/Pages/SheepManager.tsx
@@ -1,11 +1,12 @@
 import { Suspense, SyntheticEvent, useState, lazy, useEffect } from "react"
-import { useDB, useFind } from "react-pouchdb"
+import { useDB } from "react-pouchdb"
 import Calendar from "react-calendar"
 import Loading from "../Components/Loading"
-import 'react-calendar/dist/Calendar.css'
-import "./SheepManager.css"
 import ShowCalendar from "../Functions/ShowCalendar"
 import HideCalendar from "../Functions/HideCalendar"
+import { read } from "fs"
+import 'react-calendar/dist/Calendar.css'
+import "./SheepManager.css"
 
 const SheepList = lazy(() => import("../Components/SheepList"))
 
@@ -27,6 +28,7 @@ export default function SheepManager() {
         dateOfBirth: new Date(),
         description: ""
     })
+    const [attachment, setAttachment] = useState<string | ArrayBuffer | null>("")
     const db = useDB('sheep_database')
 
     useEffect(() => {
@@ -51,13 +53,20 @@ export default function SheepManager() {
             [name]: value
         }))
     }
+    // console.log(attachment)
 
     function handleSubmit(event: SyntheticEvent) {
         db.put({
             _id: formData.name,
             name: formData.name,
             dateOfBirth: formData.dateOfBirth,
-            description: formData.description
+            description: formData.description,
+            _attachments: {
+                "sheeppic.jpg": {
+                    content_type: "image/jpeg",
+                    data: attachment?.slice(23)
+                }
+            }
         })
           .then(function (response: responseTypes) {
             console.log(response)
@@ -69,6 +78,30 @@ export default function SheepManager() {
         
         event.preventDefault()
     }
+    
+    // function printFile(file: File) {
+    //     const reader = new FileReader();
+    //     reader.onload = function(evt) {
+    //         console.log(evt.target.result);
+    //     };
+    //     reader.readAsText(file);
+    // }
+
+    function toBase64(file: File) {
+        let reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = () => {
+            setAttachment(reader.result as string)
+        }
+        reader.onerror = err => {
+            console.log("Error: " + err)
+        }
+    }
+
+
+    function fileChange(event: any) {
+        event.target.files[0] && toBase64(event.target.files[0])
+    }
 
     return (
         <div className="manager">
@@ -76,7 +109,7 @@ export default function SheepManager() {
                 <p>Sheep name:</p>
                 <input
                     type="string"
-                    placeholder="Sheep name"
+                    placeholder="Npr. Franka"
                     name="name"
                     value={formData.name}
                     onChange={handleChange}
@@ -84,7 +117,7 @@ export default function SheepManager() {
                     />
                 <p>Sheep description:</p>
                 <textarea 
-                    placeholder="Sheep description"
+                    placeholder="Npr. boja dlake, specifične šare, veličina..."
                     name="description"
                     value={formData.description}
                     onChange={handleChange}
@@ -100,9 +133,17 @@ export default function SheepManager() {
                 <div className="modal" id="modal">
                     <Calendar className="calendar" value={formData.dateOfBirth} onChange={handleDate} />
                 </div>
+                <p>Sheep picture:</p>
+                <input
+                    type="file"
+                    title="sheep pic"
+                    onChange={fileChange}
+                    required
+                />
                 <input
                     type="submit"
                     />
+                {attachment && <img alt="what" src={attachment as string} width="200px"></img>}
             </form>
             <Suspense fallback={<Loading />}>
                 <SheepList />
